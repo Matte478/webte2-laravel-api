c017469c04999e2cbed3f330d9c6fe7c1ef5c795
dave <david.spak478@gmail.com>
2020-05-13 21:26:10 +0200

    multilang middleware

---
+++ resources/lang/sk/frontend.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'hp' => [
+        'welcome' => 'Vitaj'
+    ]
+];
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::redirect('/', '/sk');
+
+Route::group(['prefix' => '{lang}'], function() {
+    Route::get('/', function () {
+        return view('welcome');
+    });
 });
+
