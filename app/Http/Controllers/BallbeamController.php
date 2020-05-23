b537e919fd2b9d3b645583463b9e77070f360907
dave <david.spak478@gmail.com>
2020-05-23 22:07:02 +0000

    Ballbeam WIP :construction:

---
+++ app/Http/Controllers/BallbeamController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Symfony\Component\Process\Process;
+use Illuminate\Support\Facades\Validator;
+
+class BallbeamController extends Controller
+{
+    public function index(Request $request)
+    {
+        // TODO: get last row
+        
+        $validatedData = $request->validate([
+            'r' => 'required|numeric',
+        ]);
+
+        $process = new Process(["octave", '-qf', '-W', '--eval', $this->getScript($validatedData['r'])]);
+
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            return response()->json(['error' => $process->getErrorOutput()], 200);
+        }
+
+        return response()->json(['data' => $this->parse($process->getOutput())], 200);
+    }
+
+    private function parse($response) {
+        $data = explode("angle =\n\n", $response);
+
+        $data[0] = str_replace(' ', '', $data[0]);
+
+        $secondPart = explode("lastX =\n\n", $data[1]);
+        $secondPart[0] = str_replace(' ', '', $secondPart[0]);
+
+        $secondPart[1] = rtrim($secondPart[1], "\n\n");
+        $secondPart[1] = explode(' ', $secondPart[1]);
+
+        $lastX = array_filter($secondPart[1]);
+        $position = explode("\n", $data[0]);
+        $angle = explode("\n", $secondPart[0]);
+
+        return [
+            'position' => array_filter($position),
+            'angle' => array_filter($angle),
+            'lastX' => array_values($lastX)
+        ];
+    }
+
+    private function getScript($r) {
+        return '
+            pkg load control;
+            m = 0.111;
+            R = 0.015;
+            g = -9.8;
+            J = 9.99e-6;
+            H = -m*g/(J/(R^2)+m);
+            A = [0 1 0 0; 0 0 H 0; 0 0 0 1; 0 0 0 0];
+            B = [0;0;0;1];
+            C = [1 0 0 0];
+            D = [0];   
+            K = place(A,B,[-2+2i,-2-2i,-20,-80]);
+            N = -inv(C*inv(A-B*K)*B);
+            
+            sys = ss(A-B*K,B,C,D);
+            
+            t = 0:0.01:5;
+            r = ' . $r . ';
+            initRychlost=0;
+            initZrychlenie=0;
+            [y,t,x]=lsim(N*sys,r*ones(size(t)),t,[initRychlost;0;initZrychlenie;0]);
+            
+            disp(N*x(:,1))
+            
+            angle = x(:,3)
+            
+            lastX = x(size(x,1),:)
+            ';
+    }
+}
